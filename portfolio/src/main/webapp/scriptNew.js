6c99ffc351f126211559d6d59f103b2d017a223e
Laura Camacho <lcamac02@calpoly.edu>
2021-06-22 04:42:01 +0000

    JSON tutorial complete bb

--- portfolio/src/main/webapp/scriptNew.js
+++ portfolio/src/main/webapp/scriptNew.js
@@ -12,6 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+async function getFacts() {
+  const responseFromServer = await fetch('/funFacts');
+  // The json() function returns an object that contains fields that we can
+  // reference to create HTML.
+  const facts = await responseFromServer.json();
+    console.log("bruh");
+  const factsListElement = document.getElementById('fun-facts-container');
+  const faxx = [facts.fact1, facts.fact2, facts.fact3];
+  let bruh = Math.floor(Math.random() * 3);
+  factsListElement.innerHTML = faxx[bruh];
+
+  /*factsListElement.appendChild(
+      createListElement(facts.fact1));*/
+}
+
+/** Creates an <li> element containing text. */
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
+}
+
 async function showMessage() {
   const responseFromServer = await fetch('/message');
   const textFromResponse = await responseFromServer.text();
